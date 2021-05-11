85d971090c4894f757b8f511189e5e6192a427ff
ALİ TAHİR ELES <alieles@stu.aydin.edu.tr>
2021-05-11 23:59:25 +0300

    First commit

---
+++ HomePage.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace EmployeTrackingSystem
+{
+   
+    
+    public partial class HomePage : System.Web.UI.Page
+    {
+
+        MySqlConnection conn = new MySqlConnection(@"server=127.0.0.1;user id=alieles;database=ets; password=mymind3278.");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+       
+
+        protected void btn_login_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand("Select * from patron where username=@P1 and password=@P2", conn);
+            cmd.Parameters.AddWithValue("@P1", usernametext.Text);
+            cmd.Parameters.AddWithValue("@P2", passwordtext.Text);
+            MySqlDataReader dr = cmd.ExecuteReader();
+            if(dr.Read())
+            {
+                Response.Redirect("ControlPanel_Page.aspx");
+            }
+            else
+            {
+                // Response.Write("Failed to Login, try again!");
+                //ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Record Inserted Successfully')", true);
+                Response.Write("<script>alert('failed to login')</script>");
+            }
+        }
+
+        protected void btn_register_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("RegisterPage.aspx");
+        }
+    }
+}
---
+++ HomePage.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik üretildi>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler yanlış davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik üretildi>
+//------------------------------------------------------------------------------
+
+namespace EmployeTrackingSystem {
+    
+    
+    public partial class HomePage {
+        
+        /// <summary>
+        /// usernametext denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox usernametext;
+        
+        /// <summary>
+        /// passwordtext denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox passwordtext;
+        
+        /// <summary>
+        /// btn_login denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_login;
+        
+        /// <summary>
+        /// btn_register denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_register;
+    }
+}
